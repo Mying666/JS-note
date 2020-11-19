2801b35f8c49de71756fb3518b5f6790dbd934d3
郭洪亮 <guohongliang@9fbank.com.cn>
2020-11-19 14:35:49 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 删除原文件
+rm -rf docs/index.html docs/assets/ docs/404.html
+
+# 生成静态文件
+npm run build
+
+mv ./docs/.vuepress/dist/* ./docs/
+
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/Mying666/JS-note.git master
+
+cd -
