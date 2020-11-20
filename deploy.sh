25fb799b81de6fcd2da1324bb07eaf121cc24c59
郭洪亮 <guohongliang@9fbank.com.cn>
2020-11-20 13:53:46 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,19 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+git checkout gh-pages
+
 # 删除原文件
-rm -rf docs/index.html docs/assets/ docs/404.html
+rm -rf index.html assets/ 404.html
 
 # 生成静态文件
-npm run build
+# npm run build
 
-mv ./docs/.vuepress/dist/* ./docs/
+mv ./docs/.vuepress/dist/* ./
 
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Mying666/JS-note.git master
+# git push -f git@github.com:Mying666/JS-note.git master
+git checkout master
 
 cd -
